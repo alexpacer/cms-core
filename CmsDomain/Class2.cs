ea8b5e72285f362400167f53703606b959ff7d18
Alex Wei <alex.wei@afusion.com>
2014-04-18 11:35:06 +0800

    added xml file

---
+++ CmsDomain/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsDomain
+{
+    /// <summary>
+    /// class added by cms-project1 developers
+    /// </summary>
+    public class Class2
+    {
+    }
+}
